8162d0b6eafec766409aba6ff9b755e4e731608c
Glendon Holst <glendon.holst@kaust.edu.sa>
2016-03-03 12:18:18 +0300

    tweaked build scripts to work with new builds of ParaView and on Shaheen.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh -e
 module add kvl-applications paraview/4.4.0-mpich-x86_64
 
+# Needed because paraview/4.4.0-mpich-x86_64 doesn't set PYTHONPATHs correctly 
+module add dev-inshimtu
+
 export INSHIMTU_DIR=$(cd `dirname $0` && pwd)
 
 
