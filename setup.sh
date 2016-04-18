2c92152bb4daec067104a6f58ef99fd642ecb57d
Glendon Holst <glendon.holst@kaust.edu.sa>
2016-04-18 10:36:45 +0300

    Launch script now demostrates 4 node, ramdisk based, live-processing on Shaheen Quoted some script variables; Attempted to add support for Cray perf tools (didn't work, commented out, but left notes to try and fix in future).

--- setup.sh
+++ setup.sh
@@ -25,6 +25,11 @@ case "$OSVERSION" in
   #module add paraview/4.4.0 
 
   module sw PrgEnv-cray PrgEnv-gnu
+
+  # Enable profiling tools
+  module unload darshan
+  module load perftools-base/6.3.2 perftools/6.3.2
+
   module add cmake/3.1.3
   module add cray-hdf5
 
@@ -35,7 +40,7 @@ case "$OSVERSION" in
   # > module sw PrgEnv-cray PrgEnv-gnu
   module sw PrgEnv-gnu PrgEnv-cray
   module add paraview/4.3.1
-
+  
   echo "Setting Inshimtu build directory: ${INSHIMTU_DIR}/build"
   mkdir "${INSHIMTU_DIR}/build.shaheen"
   cd "${INSHIMTU_DIR}/build.shaheen"
@@ -43,6 +48,11 @@ case "$OSVERSION" in
   cmake -DMPI_C_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_CXX_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_C_LIBRARIES="${MPICH_DIR}/lib" -DMPI_CXX_LIBRARIES="${MPICH_DIR}/lib" -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which g++)" ..
 
   make -j 8
+
+  # NOTE: Profiling Disabled
+  # TODO: Fix ERROR: Missing required ELF section '.note.link' from the program 'Inshimtu'. Load the correct 'perftools' module and rebuild the program.
+  # NOTE: This error can indicate that perftools doesn't like the directory name (e.g., has a 'tmp' in it).
+  #pat_build -S Inshimtu
 
   ;;
 *)
