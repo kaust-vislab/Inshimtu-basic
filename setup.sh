9566a1bab06c1c77aea2dd7edc6e0d7e39c0d91b
Glendon Holst <glendon.holst@kaust.edu.sa>
2016-09-27 15:41:03 +0300

    Upgraded to ParaView 5.2.1 support, and CMake v3.5; cleaned up older cruft, and made shaheen build cleaner

--- setup.sh
+++ setup.sh
@@ -3,46 +3,43 @@
 export INSHIMTU_DIR=$(cd `dirname $0` && pwd)
 
 OSVERSION=$(echo `lsb_release -sir` | awk -F '.' '{ print $1 }')
+
 case "$OSVERSION" in
 "CentOS"*)
-  module add kvl-applications paraview/4.4.0-mpich-x86_64
+  module add kvl-applications paraview/5.1.2-mpich-x86_64
 
   echo "Setting Inshimtu build directory: ${INSHIMTU_DIR}/build.kvl"
   mkdir "${INSHIMTU_DIR}/build.kvl"
   cd "${INSHIMTU_DIR}/build.kvl"
 
-  cmake ..
+  cmake3 ..
 
   make -j 8
 
   ;;
 "SUSE LINUX"*)
 
-  # TODO: doesn't build against paraview/4.4.0; possibly not built with GCC?; has link error with std:: libs
-  #module add paraview/4.4.0 
-
-  module sw PrgEnv-cray PrgEnv-gnu
+  # NOTE: Build on compute node:
+  # salloc
+  # srun bash -i
+  # cd /lustre/project/k1033/Development/Inshimtu
+  # setup.sh
 
   # Enable profiling tools
-  module unload darshan
-  module load perftools-base/6.3.2 perftools/6.3.2
+  #module unload darshan
+  #module load perftools-base/6.3.2 perftools/6.3.2
 
-  module add cmake/3.1.3
-  module add cray-hdf5
-
-  # Note: Issue is that loading boost changes gcc from gcc/5.1.0 to gcc/4.9.3
-  module add boost
-
-  # Implied by paraview
-  # > module sw PrgEnv-cray PrgEnv-gnu
-  module sw PrgEnv-gnu PrgEnv-cray
-  module add paraview/4.3.1
+  module use /lustre/project/k1033/software/easybuild/modules/all
+  module add CMake
+  module add Boost
+  module add ParaView/5.1.2-CrayGNU-2016.07.KSL-server-mesa
+  module add cray-hdf5-parallel
   
   echo "Setting Inshimtu build directory: ${INSHIMTU_DIR}/build.shaheen"
   mkdir "${INSHIMTU_DIR}/build.shaheen"
   cd "${INSHIMTU_DIR}/build.shaheen"
 
-  cmake -DMPI_C_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_CXX_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_C_LIBRARIES="${MPICH_DIR}/lib" -DMPI_CXX_LIBRARIES="${MPICH_DIR}/lib" -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which g++)" ..
+  cmake -DMPI_C_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_CXX_INCLUDE_PATH="${MPICH_DIR}/include" -DMPI_C_LIBRARIES="${MPICH_DIR}/lib" -DMPI_CXX_LIBRARIES="${MPICH_DIR}/lib" -DCMAKE_C_COMPILER="$(which gcc)" -DCMAKE_CXX_COMPILER="$(which g++)" -DBOOST_ROOT=$EBROOTBOOST ..
 
   make -j 8
 
